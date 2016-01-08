6e9851b28e00b60f62aa09cfe257d49200b60a91
wieselsoft <stadtmuellertim@gmail.com>
2016-01-08 23:07:10 +0100

    Updated install.sh

--- install.sh
+++ install.sh
@@ -6,13 +6,59 @@ if [ $( id -u ) -gt 0 ]
    echo "      Try sudo.";
    exit 1;
 fi
+
     
 echo "Enter FTP-Directorie: ";
 read ftpDir;
+logPath = "$ftpDir/SpcWtch/"
+swHome = "$HOME/SpcWtch/"
 
-echo "Creating directories..."
+
+echo "Creating directories...";
 mkdir $ftpDir;
-mkdir "$ftpDir/SpcWtch/";
-mkdir "$HOME/SpcWtch/";
+mkdir $logPath;
+mkdir $swHome;
 
-echo "Moving Files"
+
+echo "Copy Files...";
+cp ./clearFiles.py $swHome;
+cp ./sw.py $swHome;
+touch $logPath/mesg.txt;
+
+
+echo "Installing ProFTP...";
+apt-get install proftp;
+
+
+echo "Configuring ProFTP...";
+file = /etc/proftpd/proftpd.conf
+echo "DefaultRoot ~" >> $file;
+echo "AuthOrder              mod_auth_file.c  mod_auth_unix.c" >> $file;
+echo "AuthUserFile /etc/proftpd/ftpd.passwd" >> $file;
+echo "AuthPAM off" >> $file;
+echo "RequireValidShell off" >> $file;
+
+
+echo "Creating user for FTP-Access...";
+echo "Enter a user name:( Required for login ): ";
+read $userName;
+cd /etc/proftpd/;
+uid=$( id www-data -u )
+gid=$( id www-data -g )
+ftpasswd -passwd -name $userName -uid $uid -gid $gid -home $ftpDir -shell /bin/false;
+chmod g+s $ftpDir;
+chmod 755 $ftpDir
+chown -R www-data:www-data $ftpDir
+
+
+echo "Restarting FTP-Server..."
+/etc/init.d/proftpd restart;
+
+
+echo "Setup finished. FTP-Server is ready to use."
+ip=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
+echo "Try to call ftp://$ip from your browser."
+
+
+
+
