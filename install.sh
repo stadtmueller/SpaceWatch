c8767f6bca8ac70c7d710cd7d1f3d9b414329b06
wieselsoft <stadtmuellertim@gmail.com>
2016-01-11 23:53:21 +0100

    Added config file input in install script

--- install.sh
+++ install.sh
@@ -8,40 +8,36 @@ if [ $( id -u ) -gt 0 ]
 fi
 
     
-echo "Enter FTP-Directory ( No '/' at the end ): ";
+echo "Enter FTP-Directorie: ";
 read ftpDir;
-logPath="$ftpDir/SpcWtch/"
-swHome="$HOME/SpcWtch/"
-echo "";
+logPath="$ftpDir/SpcWtch"
+swHome="$HOME/SpcWtch"
 
 
 echo "Creating directories...";
 mkdir $ftpDir;
 mkdir $logPath;
 mkdir $swHome;
-echo "";
 
 
 echo "Copy Files...";
-cp src/clearFiles.py $swHome;
-cp src/sw.py $swHome;
+cp ./clearFiles.py $swHome;
+cp ./sw.py $swHome;
 touch $logPath/mesg.txt;
-echo "";
+touch $swHome/sw.conf;
 
 
 echo "Installing ProFTP...";
-apt-get install proftpd;
-echo "";
+apt-get install proftp;
 
 
 echo "Configuring ProFTP...";
-file=/etc/proftpd/proftpd.conf
+file = /etc/proftpd/proftpd.conf
 echo "DefaultRoot ~" >> $file;
 echo "AuthOrder              mod_auth_file.c  mod_auth_unix.c" >> $file;
 echo "AuthUserFile /etc/proftpd/ftpd.passwd" >> $file;
 echo "AuthPAM off" >> $file;
 echo "RequireValidShell off" >> $file;
-echo "";
 
 
 echo "Creating user for FTP-Access...";
@@ -54,15 +50,32 @@ ftpasswd -passwd -name $userName -uid $uid -gid $gid -home $ftpDir -shell /bin/f
 chmod g+s $ftpDir;
 chmod 755 $ftpDir
 chown -R www-data:www-data $ftpDir
-echo "";
 
 
 echo "Restarting FTP-Server..."
 /etc/init.d/proftpd restart;
-echo "";
+
+
+echo "Configuring sw...";
+
+echo "Enter minimum available size";
+read min;
+
+echo "Enter a list of mail addresses that have to be notified when running out of space.";
+echo "Seperate with ','";
+read mailinglist;
+
+configFile="$swHome/sw.conf"
+
+echo "ftpDir = $ftpDir" >> $configFile;
+echo "mailinglist = $mailinglist" >> $configFile;
+echo "min = $min" >> $configFile;
+
+echo ""
+
 
 
 echo "Setup finished. FTP-Server is ready to use."
 ip=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
 echo "Try to call ftp://$ip from your browser."
-echo ""
+
