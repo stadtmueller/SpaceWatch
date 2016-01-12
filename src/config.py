19962cd5b52fa8067b0e9b112dcf1eca2cedbfd6
stadtmueller <stadtmuellertim@gmail.com>
2016-01-12 12:53:24 +0100

    Added function to read the configuration

--- src/config.py
+++ src/config.py
@@ -1,1 +1,14 @@
 #! /usr/bin/python
+
+def readConfig():
+    d = {}
+    with open( "sw.conf", "r" ) as configFile:
+        for line in configFile:
+            if not line.rstrip() or line.rstrip()[0] == '#': # Skip empty lines and lines
+                continue                                     # lines starting with '#'
+
+            seperatorPos = line.find( '=' )
+            name = line[ :seperatorPos ].strip()
+            content = line[ seperatorPos + 1: ].strip()
+            d.update( { name : content } )
+    return d
