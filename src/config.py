87820afc9fd102440c04bff555b03f9a0277cc7c
stadtmueller <stadtmuellertim@gmail.com>
2016-02-11 12:40:29 +0100

    Added value checking to config.py

--- src/config.py
+++ src/config.py
@@ -1,5 +1,12 @@
 #! /usr/bin/python
 
+def isInt( txt ):
+    try: 
+        int( txt )
+        return True
+    except ValueError:
+        return False
+
 def readConfig():
     d = {}
     with open( "/usr/local/SpcWtch/sw.conf", "r" ) as configFile:
@@ -11,4 +18,26 @@ def readConfig():
             name = line[ :seperatorPos ].strip()
             content = line[ seperatorPos + 1: ].strip()
             d.update( { name : content } )
+            
+    if not d[ "unit" ] in "KkB":
+        d[ "unit" ] = "K"
+        print( "Error in config file detected. Setting unit to K." )
+    if not d[ "messaging" ] in "nwd":
+        d[ "messaging" ] = "n"
+        print( "Error in config file detected. Setting messaging to never." )
+    
+    if not isInt( d[ "min" ] ):
+        d[ "min" ] = "5000000"
+        print( "Error in config file detected. Setting min to 5.000.000." )
+
+    if isInt( d[ "mailtime" ] ):
+        timeInt = int( d[ "mailtime" ] )
+        if timeInt < 0 or timeInt > 23:
+            d[ "mailtime" ] = "18"
+            print( "Error in config file detected. Setting mailtime to 18" )
+    if not isInt( d[ "mailtime" ] ):
+        d[ "mailtime" ] = "18"
+        print( "Error in config file detected. Setting mailtime to 18" )
+
+
     return d
